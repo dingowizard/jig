12a50a4e69213dd929a7689eec879c2c23c42ba8
Dave Brda <Dave Brda>
2023-11-29 06:43:07 -0500

    User Expr.Symbol rather than string in Environment

--- Jig/Program.cs
+++ Jig/Program.cs
@@ -41,54 +41,51 @@ public static class Program {
 public class Environment : IEnvironment {
 
     public Environment() {
-        // TODO: why are we using strings rather than symbols?
-        _dict.Add("car", new LiteralExpr<Delegate>((Builtin) Builtins.car));
-        _dict.Add("cdr", new LiteralExpr<Delegate>((Builtin) Builtins.cdr));
-        _dict.Add("cons", new LiteralExpr<Delegate>((Builtin) Builtins.cons));
-        _dict.Add("null?", new LiteralExpr<Delegate>((Builtin) Builtins.nullP));
-        _dict.Add("succ", new LiteralExpr<Delegate>((Builtin) Builtins.succ));
-        _dict.Add("+", new LiteralExpr<Delegate>((Builtin) Builtins.sum));
-        _dict.Add("*", new LiteralExpr<Delegate>((Builtin) Builtins.product));
-        _dict.Add("-", new LiteralExpr<Delegate>((PairFunction) Builtins.diff));
-        _dict.Add("=", new LiteralExpr<Delegate>((PairFunction) Builtins.numEq));
-        _dict.Add("apply", new LiteralExpr<Delegate>( Builtins.apply));
+        _dict.Add(new Expr.Symbol("car"), new LiteralExpr<Delegate>((Builtin) Builtins.car));
+        _dict.Add(new Expr.Symbol("cdr"), new LiteralExpr<Delegate>((Builtin) Builtins.cdr));
+        _dict.Add(new Expr.Symbol("cons"), new LiteralExpr<Delegate>((Builtin) Builtins.cons));
+        _dict.Add(new Expr.Symbol("null?"), new LiteralExpr<Delegate>((Builtin) Builtins.nullP));
+        _dict.Add(new Expr.Symbol("succ"), new LiteralExpr<Delegate>((Builtin) Builtins.succ));
+        _dict.Add(new Expr.Symbol("+"), new LiteralExpr<Delegate>((Builtin) Builtins.sum));
+        _dict.Add(new Expr.Symbol("*"), new LiteralExpr<Delegate>((Builtin) Builtins.product));
+        _dict.Add(new Expr.Symbol("-"), new LiteralExpr<Delegate>((PairFunction) Builtins.diff));
+        _dict.Add(new Expr.Symbol("="), new LiteralExpr<Delegate>((PairFunction) Builtins.numEq));
+        _dict.Add(new Expr.Symbol("apply"), new LiteralExpr<Delegate>( Builtins.apply));
 
     }
 
     public void Set(Continuation k, Expr sym, Expr v) {
-        string name = sym is SyntaxObject.Identifier id ? id.Symbol.Name : ((Expr.Symbol) sym).Name;
         Expr.Symbol s = sym is SyntaxObject.Identifier i ? i.Symbol : ((Expr.Symbol) sym);
-        if (!_dict.ContainsKey(name)) {
+        if (!_dict.ContainsKey(s)) {
             throw new Exception($"set!: unbound variable {s}");
         }
-        _dict[name] = v;
+        _dict[s] = v;
         k(s);
         return;
 
     }
 
     public void Define (Continuation k, Expr sym, Expr v) {
-        string name = sym is SyntaxObject.Identifier id ? id.Symbol.Name : ((Expr.Symbol) sym).Name;
         Expr.Symbol s = sym is SyntaxObject.Identifier i ? i.Symbol : ((Expr.Symbol) sym);
-        if (_dict.ContainsKey(name)) {
-            _dict[name] = v;
+        if (_dict.ContainsKey(s)) {
+            _dict[s] = v;
             k(s);
             return;
         }
-        _dict.Add(name, v);
+        _dict.Add(s, v);
         k(s);
         return;
     }
 
     public void LookUp (Continuation k, Expr expr) {
         Expr.Symbol symbol = expr is SyntaxObject.Identifier id ? id.Symbol : (Expr.Symbol) expr;
-        if (_dict.TryGetValue(symbol.Name, out Expr? result)) {
+        if (_dict.TryGetValue(symbol, out Expr? result)) {
             k(result);
         } else {
             throw new Exception($"unbound variable: {symbol.Name}");
         }
     }
 
-    Dictionary<string, Expr> _dict = new Dictionary<string,Expr>();
+    Dictionary<Expr.Symbol, Expr> _dict = new Dictionary<Expr.Symbol,Expr>();
 
 }
