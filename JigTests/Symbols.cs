a0cdad79a0c1d85a79cb1aafec9d719cd2cf89b5
Dave Brda <davebrda@gmail.com>
2024-04-01 07:50:41 -0400

    Initial commit

---
+++ JigTests/Symbols.cs
@@ -0,0 +1,35 @@
+namespace JigTests;
+
+[TestClass]
+public class Symbols
+{
+
+    [TestMethod]
+    [DataRow("(symbol? 'boo)", "#t")]
+    [DataRow("(symbol? 12)", "#f")]
+    public void SymbolP(string input, string expected)
+    {
+        var actual = Utilities.InterpretUsingReadSyntax(input);
+        Assert.AreEqual(actual, expected);
+
+    }
+
+    [TestMethod]
+    [DataRow("(symbol->string 'boo)", "\"boo\"")]
+    public void SymbolToString(string input, string expected)
+    {
+        var actual = Utilities.InterpretUsingReadSyntax(input);
+        Assert.AreEqual(actual, expected);
+
+    }
+
+    [TestMethod]
+    [DataRow("(symbol=? 'boo 'boo)", "#t")]
+    [DataRow("(symbol=? 'boo 'bop)", "#f")]
+    public void SymbolEquals(string input, string expected)
+    {
+        var actual = Utilities.InterpretUsingReadSyntax(input);
+        Assert.AreEqual(actual, expected);
+
+    }
+}
