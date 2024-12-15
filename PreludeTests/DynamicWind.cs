dc97e79af74776dccc6c9a6eec41577b954b2a6c
Dave Brda <davebrda@gmail.com>
2024-12-15 15:43:03 -0500

    Add tests for parameters

--- PreludeTests/DynamicWind.cs
+++ PreludeTests/DynamicWind.cs
@@ -7,12 +7,42 @@ public class DynamicWind
 
     [TestMethod]
     public void ReturnsInThunkDoesBeforeAndAfterThunks() {
-        string actual = Utilities.PreludeInterp.InterpretSequenceReadSyntax(new string[] {
-                "(define b 2)",
+        var actual = Utilities.PreludeInterp.InterpretSequenceReadSyntax([
+            "(define b 2)",
                 "(define in (dynamic-wind (lambda () (set! b (+ b 1))) (lambda () b) (lambda () (set! b (+ b 1)))))",
                 "(+ in b)"
-            });
+        ]);
         Assert.AreEqual("7", actual);
-
     }
+
+    [TestMethod]
+    public void MakeAndUseParameters() {
+        var actual = Utilities.PreludeInterp.InterpretSequenceReadSyntax([
+            "(define p (make-parameter 1))",
+                "(define q (make-parameter 2))",
+                "(+ (p) (q))"
+        ]);
+        Assert.AreEqual("3", actual);
+    }
+    
+    [TestMethod]
+    public void Parameterize() {
+        var actual = Utilities.PreludeInterp.InterpretSequenceReadSyntax([
+            "(define p (make-parameter 1))",
+            "(define q (make-parameter 2))",
+            "(cons (parameterize ((p 10) (q 20)) (+ (p) (q))) (+ (p) (q)))"
+        ]);
+        Assert.AreEqual("(30 . 3)", actual);
+    }
+
+    [TestMethod]
+    public void ParameterizeParameterInFunction() {
+        var actual = Utilities.PreludeInterp.InterpretSequenceReadSyntax([
+            "(define p (make-parameter 1))",
+            "(define foo (lambda () (p)))",
+            "(cons (parameterize ((p 10)) (foo)) (foo))"
+        ]);
+        Assert.AreEqual("(10 . 1)", actual);
+    }
+    
 }
