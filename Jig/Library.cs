be66a01179bb36726332030343b0f52025f87c76
Dave Brda <davebrda@gmail.com>
2025-01-28 09:48:53 -0500

    ?

--- Jig/Library.cs
+++ Jig/Library.cs
@@ -2,4 +2,8 @@ namespace Jig;
 
 public class Library { // TODO: should it inherit?
     
+    private Environment env { get; }
+    
+    public Tuple<Form.Symbol, Form>[] Exports { get; private set; }
+    
 }
